db5c86d00472ef3bf73d81d7ce1d7ada216bdfc9
clark_b <bish@city>
2020-01-11 19:54:42 -0800

    0.2.0 * start to incorporate RPMs for enterprise linux * deal with wrong file locations from RPM * systemd is a squishy blob without consistent/compliant locations also.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 # CHANGELOG
 
+## 0.2.0 (2020-01-11)
+
+IMPROVEMENTS:
+
+* start to incorporate RPMs for enterprise linux
+* systemd is a squishy blob without consistent/compliant locations.
+* deal with sub-optimal data dir location
+* prefer standard conf file location, but allow for wrong people
+
 ## 0.1.1 (2018-05-06)
 
 IMPROVEMENTS:
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
-name 'minio-server'
-maintainer 'Takaaki Furukawa'
+name		 'minio-server'
+maintainer	 'Takaaki Furukawa'
 maintainer_email 'takaaki.frkw@gmail.com'
-license 'MIT'
-description 'Installs/Configures Minio'
+license		 'MIT'
+description	 'Installs/Configures Minio'
 long_description 'Installs/Configures Minio'
-version '0.1.1'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version	 '>= 12.14' if respond_to?(:chef_version)
+
+version		 '0.2.0'
 
 supports 'redhat', '>= 7.0'
 supports 'centos', '>= 7.0'
